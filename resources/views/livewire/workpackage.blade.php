8279d2d2c0743415e9da521c8c7a376fdefbc221
Jordan Elphick <jelphick@zenviron.com>
2023-04-26 16:50:05 +1000

    minor changes to workpackages view

--- resources/views/livewire/workpackage.blade.php
+++ resources/views/livewire/workpackage.blade.php
@@ -125,7 +125,7 @@
 
                             <div class="border-t border-white/10">
 
-                                <table class="mt-6 w-full whitespace-nowrap text-left">
+                                <table class="mt-6 w-full text-left table-auto">
                                     <colgroup>
                                         <col class="w-full sm:w-4/12">
                                         <col class="lg:w-4/12">
@@ -135,42 +135,43 @@
                                     </colgroup>
                                     <thead class="border-b border-white/10 text-sm leading-6 text-gray-800">
                                     <tr>
-                                        <th scope="col" class="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8">User</th>
-                                        <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold sm:table-cell">Commit</th>
+                                        <th scope="col" class="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8">Record</th>
+                                        <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold sm:table-cell">Comment</th>
                                         <th scope="col" class="py-2 pl-0 pr-4 text-right font-semibold sm:pr-8 sm:text-left lg:pr-20">Status</th>
                                         <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20">Duration</th>
-                                        <th scope="col" class="hidden py-2 pl-0 pr-4 text-right font-semibold sm:table-cell sm:pr-6 lg:pr-8">Deployed at</th>
+                                        <th scope="col" class="hidden py-2 pl-0 pr-4 text-right font-semibold sm:table-cell sm:pr-6 lg:pr-8">Date Reviewed</th>
                                     </tr>
                                     </thead>
                                     <tbody class="divide-y divide-white/5">
-                                    <tr>
-                                        <td class="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                                            <div class="flex items-center gap-x-4">
-                                                <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-8 w-8 rounded-full bg-gray-800">
-                                                <div class="truncate text-sm font-medium leading-6 text-gray-800">Michael Foster</div>
-                                            </div>
-                                        </td>
-                                        <td class="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
-                                            <div class="flex gap-x-3">
-                                                <div class="font-mono text-sm leading-6 text-gray-400">2d89f0c8</div>
-                                                <span class="inline-flex items-center rounded-md bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-gray-400/20">main</span>
-                                            </div>
-                                        </td>
-                                        <td class="py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20">
-                                            <div class="flex items-center justify-end gap-x-2 sm:justify-start">
-                                                <time class="text-gray-400 sm:hidden" datetime="2023-01-23T11:00">45 minutes ago</time>
-                                                <div class="flex-none rounded-full p-1 text-green-400 bg-green-400/10">
-                                                    <div class="h-1.5 w-1.5 rounded-full bg-current"></div>
-                                                </div>
-                                                <div class="hidden text-gray-800 sm:block">Completed</div>
-                                            </div>
-                                        </td>
-                                        <td class="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">25s</td>
-                                        <td class="hidden py-4 pl-0 pr-4 text-right text-sm leading-6 text-gray-400 sm:table-cell sm:pr-6 lg:pr-8">
-                                            <time datetime="2023-01-23T11:00">45 minutes ago</time>
-                                        </td>
-                                    </tr>
-
+                                        @foreach($workpackage->records->where('status', 'open') as $record)
+                                            <tr>
+                                                <td class="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                                                    <div class="flex items-center gap-x-4">
+                                                        
+                                                        <div class="truncate text-sm font-medium leading-6 text-gray-800 break-words">{{ $record->comment}}</div>
+                                                    </div>
+                                                </td>
+                                                <td class="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
+                                                    <div class="flex gap-x-3">
+                                                        <div class="font-mono text-sm leading-6 text-gray-400">{{ $record->document_number}}</div>
+                                                        <span class="inline-flex items-center rounded-md bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-gray-400/20">{{ $record->document_revision}}</span>
+                                                    </div>
+                                                </td>
+                                                <td class="py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20">
+                                                    <div class="flex items-center justify-end gap-x-2 sm:justify-start">
+                                                        <time class="text-gray-400 sm:hidden" datetime="2023-01-23T11:00">45 minutes ago</time>
+                                                        <div class="flex-none rounded-full p-1 text-green-400 bg-green-400/10">
+                                                            <div class="h-1.5 w-1.5 rounded-full bg-current"></div>
+                                                        </div>
+                                                        <div class="hidden text-gray-800 sm:block">{{$record->status}}</div>
+                                                    </div>
+                                                </td>
+                                                <td class="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">{{$record->date_reviewed}}</td>
+                                                <td class="hidden py-4 pl-0 pr-4 text-right text-sm leading-6 text-gray-400 sm:table-cell sm:pr-6 lg:pr-8">
+                                                    <time datetime="2023-01-23T11:00">{{$record->date_reviewed}}</time>
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     <!-- More items... -->
                                     </tbody>
                                 </table>
